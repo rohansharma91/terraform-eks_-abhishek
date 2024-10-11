b7cd375fe1bd073fa418bc68b5a93a0742c7fc6a
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-10-11 16:01:36 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -21,8 +21,8 @@ module "vpc" {
   name                 = "abhi-eks-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
